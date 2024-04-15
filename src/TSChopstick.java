b9a1a406cd99020adb324a86c95becbfd245cc3e
cwdatlas <ascott@carroll.edu>
2024-04-14 20:53:28 -0600

    added comments and header

--- src/TSChopstick.java
+++ src/TSChopstick.java
@@ -1,14 +1,28 @@
 package src;
 
+/**
+ * @author Aidan Scott and Hank Rugg
+ * TSChopstick is used to provide safe concurrent access to the chopstick object.
+ * Implimentation is busy mutex
+ */
 public class TSChopstick {
 
     private final String name;
     private volatile boolean Used = false;
 
+    /**
+     * TSChopstick sets the name of the chopstick. Chopstick starts as not used
+     * Acquire to use the chopstick
+     * release to release the chopstick, if chopstick is not acquired, release will throw a runtime exception.
+     * @param name
+     */
     public TSChopstick(String name) {
         this.name = name;
     }
-
+    /**
+     * acquire will allow a user to acquire the chopstick.
+     * it will not be allowed to be used by any other thread until it is released
+     */
     public synchronized boolean acquire() {
         // if it's not being used, change it to being used and return true
         if (this.Used) {
@@ -19,6 +33,11 @@ public class TSChopstick {
         }
     }
 
+    /**
+     * release will allow the chopstick to be acquired again.
+     * if chopstick is not acquired when released, then release will throw a Runtime Exception
+     * @throws RuntimeException
+     */
     public synchronized void release() {
         //change being used to false when released
         if (Used) {
@@ -28,11 +47,11 @@ public class TSChopstick {
         }
     }
 
+    /**
+     * isUsed returns the current status of the chopstick
+     * @return status of chopstick
+     */
     public boolean isUsed() {
         return Used;
-    }
-
-    public String toString() {
-        return name;
     }
 }
